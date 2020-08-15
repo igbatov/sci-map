2138bf695ac08d1caea1b9bfc33b07f330cd236f
Igor Batov <igbatov@ozon.ru>
2020-08-15 20:43:09 +0300

    Added style to wiki text

--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -1,5 +1,7 @@
 <template>
-  <div v-if="content" class="infoBox" v-html="content"/>
+  <transition name="slide">
+    <div v-if="content" class="infoBox" v-html="content"/>
+  </transition>
 </template>
 
 <script>
@@ -15,11 +17,17 @@ export default {
 
 <style scoped>
   .infoBox {
+    padding: 20px;
     position: absolute;
     width: 33%;
     height: 100%;
     background-color: white;
     overflow: scroll;
     box-shadow: 5px 0 21px 3px rgba(122, 122, 119, 0.50);
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transform: translateX(0);
+    transition: transform .3s .1s, opacity .3s .1s, width .3s;
   }
 </style>
--- src/store/infoBox.js
+++ src/store/infoBox.js
@@ -36,6 +36,10 @@ export default {
       const wikiPage = node.wiki.substr("https://en.wikipedia.org/wiki/".length);
       const data = await axios.get('https://en.wikipedia.org/w/api.php?action=parse&page='+wikiPage+'&prop=text&format=json&mobileformat=true&origin=*')
       commit("SET_CONTENT", {nodeId, content: data.data.parse.text["*"]})
+
+      // swagger https://en.wikipedia.org/api/rest_v1
+      // const data = await axios.get('https://en.wikipedia.org/api/rest_v1/page/mobile-sections/'+wikiPage, {headers: {accept: "application/json", profile: "https://www.mediawiki.org/wiki/Specs/mobile-sections/0.14.1"}});
+      // commit("SET_CONTENT", {nodeId, content: data.lead.sections[0].text})
     }
   }
 };
