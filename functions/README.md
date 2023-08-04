73a8f67c0f36c0d5d155f85a94a14a35050d167a
Igor Batov <igbatov@ozon.ru>
2023-08-04 21:45:12 +0200

    added roles

--- functions/README.md
+++ functions/README.md
@@ -1,1 +1,1 @@
-Here is Firebase Functions to aggregate user's node_contents and add to general map
+Here is Firebase backend
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,21 @@
-const admin = require("firebase-admin");
-admin.initializeApp();
 
+const admin = require('firebase-admin');
+const { getAuth } = require('firebase-admin/auth');
+
+admin.initializeApp()
+getAuth()
+  .getUserByEmail('igbatov@gmail.com')
+  .then((user) => {
+    // Confirm user is verified.
+    if (user.emailVerified) {
+      // Add custom claims for additional privileges.
+      // This will be picked up by the user on token refresh or next sign in on new device.
+      return getAuth().setCustomUserClaims(user.uid, {
+        role: "map_editor",
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
