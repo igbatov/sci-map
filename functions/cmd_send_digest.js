3d305f0eafe28b7217e12770e390b78733f81e71
Igor Batov <igbatov@ozon.ru>
2024-02-20 21:32:49 +0300

    [SM-140] added cases to cmd_send_digest

--- functions/cmd_send_digest.js
+++ functions/cmd_send_digest.js
@@ -2,12 +2,75 @@ const functions = require('firebase-functions/v1');
 const nodemailer = require('nodemailer');
 const {ActionType} = require("./actions");
 const Diff = require('diff');
-const {getNodeLink} = require("./helpers");
+const {getNodeLink, getArrayDiff} = require("./helpers");
 const APP_NAME = "scimap.org"
 const USER_BATCH_LIMIT = 2
 const MAX_LOOP_LIMIT = 100_000
 const WEEK = 7*24*60*60*1000 // days*hours*minutes*seconds*1000
 const ACTIONS = [ActionType.Name, ActionType.Content, ActionType.Precondition, ActionType.ParentID, ActionType.Children, ActionType.Remove]
+
+/**
+ * getChildrenDigest
+ * @param nodeID
+ * @param nodeName
+ * @param isNodeRemoved
+ * @param added
+ * @param removed
+ * @returns {string}
+ */
+exports.getPreconditionDigest = (nodeID, nodeName, isNodeRemoved, added, removed) => {
+  let text = `Node ${getNodeLink(nodeName, nodeID, isNodeRemoved)} has`
+  if (added.length) {
+    text = `${added.length} 'based on' added`
+  }
+  if (removed.length) {
+    if (added.length) {
+      text += ' and '
+    }
+    text += `${removed.length} 'based on' removed`
+  }
+
+  return text
+}
+
+/**
+ * getChildrenDigest
+ * @param nodeID
+ * @param nodeName
+ * @param isNodeRemoved
+ * @param added
+ * @param removed
+ * @returns {string}
+ */
+exports.getChildrenDigest = (nodeID, nodeName, isNodeRemoved, added, removed) => {
+  let text = `Node ${getNodeLink(nodeName, nodeID, isNodeRemoved)} has`
+  if (added.length) {
+    text = `${added.length} children added`
+  }
+  if (removed.length) {
+    if (added.length) {
+      text += ' and '
+    }
+    text += `${removed.length} children removed`
+  }
+
+  return text
+}
+
+exports.getNodeName = async (database, nodeID) => {
+  let node = await database.ref(`/map/${nodeID}/name`).get()
+  let isNodeRemoved = false
+  if (!node.exists()) {
+    isNodeRemoved = true
+    // node was removed, take it from trash bin
+    node = await database.ref(`/trash/${nodeID}/map/name`).get()
+    if (!node.exists()) {
+      functions.logger.error("getDigest: cannot find node nor in /map nor in /trash", nodeID)
+      return [null, isNodeRemoved]
+    }
+  }
+  return [node.val(), isNodeRemoved]
+}
 
 /**
  * Filter firestore /changes to get digest for this nodeID
@@ -20,19 +83,11 @@ exports.getDigest = async (database, firestore, nodeID) => {
     return digestCache[nodeID]
   }
 
-  let node = await database.ref(`/map/${nodeID}/name`).get()
-  let isNodeRemoved = false
-  if (!node.exists()) {
-    isNodeRemoved = true
-    // node was removed, take it from trash bin
-    node = await database.ref(`/trash/${nodeID}/map/name`).get()
-    if (!node.exists()) {
-      functions.logger.error("getDigest: cannot find node nor in /map nor in /trash", nodeID)
-      digestCache[nodeID] = null
-      return
-    }
+  const [nodeName, isNodeRemoved] = exports.getNodeName(database, nodeID)
+  if (nodeName === null) {
+    digestCache[nodeID] = null
+    return
   }
-  const nodeName = node.val()
 
   for (const actionType of ACTIONS) {
     // get the latest change from the last week
@@ -45,6 +100,11 @@ exports.getDigest = async (database, firestore, nodeID) => {
       .limit(1)
 
     if (weekLastChange.docs.length === 0) {
+      continue
+    }
+
+    if (actionType === ActionType.Remove) {
+      digestCache[nodeID][actionType] = `Node ${getNodeLink(nodeName, nodeID, isNodeRemoved)} was removed`
       continue
     }
 
@@ -67,16 +127,41 @@ exports.getDigest = async (database, firestore, nodeID) => {
         .limit(1)
 
       if (prevWeekLastChange.docs[0].id === weekLastChange.docs[0].id) {
-        // It means there is only one record in the whole history,
-        // so there is no diff exists and if user subscribed this node you can bet
-        // she already saw it.
-        // So skip it
-        continue
+        // It means there is only one record in the whole history and no diff exists
+        // If a user subscribed this node and action is in [ActionType.Content, ActionType.Name, ActionType.ParentID],
+        // we think user already saw it (because she done it herself, otherwise there will be several records in changes)
+        // So skip it here
+        if ([ActionType.Content, ActionType.Name, ActionType.ParentID].indexOf(actionType) !== -1) {
+          continue
+        }
+
+        if (actionType === ActionType.Children) {
+          digestCache[nodeID][actionType] = exports.getChildrenDigest(
+            nodeID,
+            nodeName,
+            isNodeRemoved,
+            weekLastChange.docs[0].data()['attributes']['added'],
+            weekLastChange.docs[0].data()['attributes']['removed'],
+          )
+        }
+
+        if (actionType === ActionType.Precondition) {
+          digestCache[nodeID][actionType] = exports.getPreconditionDigest(
+            nodeID,
+            nodeName,
+            isNodeRemoved,
+            weekLastChange.docs[0].data()['attributes']['added'],
+            weekLastChange.docs[0].data()['attributes']['removed'],
+          )
+        }
       }
     }
 
-    // calculate the percentage of added and removed content
-    // https://github.com/kpdecker/jsdiff?tab=readme-ov-file#basic-example-in-node
+    /**
+     * Content
+     * calculate the percentage of added and removed content
+     * https://github.com/kpdecker/jsdiff?tab=readme-ov-file#basic-example-in-node
+     */
     if (actionType === ActionType.Content) {
       const diff = Diff.diffWords(
         prevWeekLastChange.docs[0].data()['attributes']['value'],
@@ -93,6 +178,57 @@ exports.getDigest = async (database, firestore, nodeID) => {
         }
       })
       digestCache[nodeID][actionType] = `Node ${getNodeLink(nodeName, nodeID, isNodeRemoved)} content changed: added ${added} words, removed ${removed} words`
+    }
+
+    /**
+     * Name
+     */
+    if (actionType === ActionType.Name) {
+      digestCache[nodeID][actionType] = `Node ${getNodeLink(nodeName, nodeID, isNodeRemoved)} was renamed from "${prevWeekLastChange.docs[0].data()['attributes']['value']}" to "${nodeName}"`
+    }
+
+    /**
+     * Children
+     */
+    if (actionType === ActionType.Children) {
+      const [added, removed] = getArrayDiff(prevWeekLastChange.docs[0].data()['attributes']['valueAfter'], weekLastChange.docs[0].data()['attributes']['valueAfter'])
+      digestCache[nodeID][actionType] = exports.getChildrenDigest(
+        nodeID,
+        nodeName,
+        isNodeRemoved,
+        added,
+        removed,
+      )
+    }
+
+    /**
+     * Precondition
+     */
+    if (actionType === ActionType.Precondition) {
+      const [added, removed] = getArrayDiff(prevWeekLastChange.docs[0].data()['attributes']['valueAfter'], weekLastChange.docs[0].data()['attributes']['valueAfter'])
+      digestCache[nodeID][actionType] = exports.getPreconditionDigest(
+        nodeID,
+        nodeName,
+        isNodeRemoved,
+        added,
+        removed,
+      )
+    }
+
+    /**
+     *
+     */
+    if (actionType === ActionType.ParentID) {
+      const parentNodeID = weekLastChange.docs[0].data()['attributes']['valueAfter']
+      if (parentNodeID === null) {
+        // this case was processed in actionType === ActionType.Remove
+        continue
+      }
+      const [parentNodeName, isParentNodeRemoved] = exports.getNodeName(database, parentNodeID)
+      if (parentNodeName === null) {
+        continue
+      }
+      digestCache[nodeID][actionType] = `Node ${getNodeLink(nodeName, nodeID, isNodeRemoved)} changed parent to ${getNodeLink(parentNodeName, parentNodeID, isParentNodeRemoved)}`
     }
   }
 }
