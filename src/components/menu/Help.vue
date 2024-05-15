63671355efac37f9bfa07d52ba067e5c4cff971a
Igor Batov <igbatov@ozon.ru>
2024-05-15 19:25:24 +0200

    added videos to Help

--- src/components/menu/Help.vue
+++ src/components/menu/Help.vue
@@ -29,10 +29,13 @@
       <br/>You can use <a target='_blank' href='https://katex.org/docs/supported.html'>katex</a> notation to write formulas.
       <br/>- To add "based on" links between nodes, press plus button.
       <br/>- To add video just insert link from youtube in a form
-      <div style="background-color: lightgrey; width: 31em;">
+      <div style="background-color: lightgrey; width: 33em; height: 2em; padding-top:0.4em; padding-left:1em;">
         @[youtube](https://www.youtube.com/watch?v=MmG2ah5Df4g)
       </div>
-      - To add add image click on header image, then "upload" and copy URL to node's content.
+      - To add add image click on header image, then "upload" and copy URL to node's content or just add any image URL in a form
+      <div style="background-color: lightgrey; width: 33em; height: 4em; padding-top:0.8em; padding-left:1em;">
+        ![](https://cdn.pixabay.com/photo/2022/11/07/21/31/rose-7577265_1280.jpg =340x)
+      </div>
       <br/>
       <br/>
       <div style="text-align:center">
@@ -41,9 +44,18 @@
       <br/>When "edit map" mode is on <img src="../../assets/images/edit-map-on.png" style="height: 2em;"/> you can add/remove new nodes but cannot edit node content.
       <br/>In this mode click on node, then click "add", "delete" or "cut" and "paste".
       <br/>To recover removed node use "log".
-
-      <br/><br/>Press "eye" button to receive node updates on email. You can receive changes daily or weekly.
-
+      <br/>
+      <br/>
+      <div style="text-align:center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/BVDFqjellcY?si=YJIl9mHFmI4QLezA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <br/>Press "eye" button to receive node updates on email. You can receive changes daily or weekly.
+      <br/>
+      <br/>
+      <div style="text-align:center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/eDrhDCze7LY?si=dFLEHd10Kw_xsKDs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <br/>
     </div>
   </Dialog>
 </template>
