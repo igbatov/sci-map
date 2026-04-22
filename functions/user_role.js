63da87d9607fff7259788fa61eb657c2015f49af
igbatov <igbatov@gmail.com>
2026-04-22 12:56:10 +0300

    added comment

--- functions/user_role.js
+++ functions/user_role.js
@@ -3,5 +3,6 @@ const realtimeDatabase =  getDatabase();
 const functions = require('firebase-functions/v1');
 
 exports.GetOnUserCreate = ()=>functions.auth.user().onCreate((user) => {
+  // permission to edit for newly registered user
   return realtimeDatabase.ref(`/user_data/${user.uid}/map_editor`).set('false')
 });
