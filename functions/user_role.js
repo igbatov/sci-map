269e1d90fe4705c901ee24ff073ee4b7003b28de
igbatov <igbatov@gmail.com>
2026-04-22 12:45:11 +0300

    map editors false by default for new users

--- functions/user_role.js
+++ functions/user_role.js
@@ -3,5 +3,5 @@ const realtimeDatabase =  getDatabase();
 const functions = require('firebase-functions/v1');
 
 exports.GetOnUserCreate = ()=>functions.auth.user().onCreate((user) => {
-  return realtimeDatabase.ref(`/user_data/${user.uid}/map_editor`).set('true')
+  return realtimeDatabase.ref(`/user_data/${user.uid}/map_editor`).set('false')
 });
